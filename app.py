d951269543dcded8376dab023d40d4415ae1e006
Kakarlapudi <satishrayapa@gmail.com>
2019-12-10 10:02:36 +0530

    change the message

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, aryan varma"
+    return "Hello, aryan rayapa varma"
     
 if __name__ == "__main__":
     app.run(debug=True)
